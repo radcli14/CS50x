b0af350ef49849faecf44d758a9df708d2322a5b
radcli14 <radcli14@gmail.com>
2025-11-08 17:08:50 +0100

    created a file to manage the database

--- project/app.py
+++ project/app.py
@@ -1,12 +1,12 @@
 import os
 
-from cs50 import SQL
 from datetime import datetime
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required
+from database import get_db, init_db
 
 # Configure application
 app = Flask(__name__)
@@ -16,9 +16,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-# TODO: use a different SQL library than the one cs50 provides
-# db = SQL("sqlite:///finance.db")
+# Initialize database connection handling
+init_db(app)
 
 @app.route("/")
 @login_required
