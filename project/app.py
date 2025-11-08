778355ba38e687b46e774625555ad541cbddede3
radcli14 <radcli14@gmail.com>
2025-11-08 16:43:03 +0100

    added content from the cs50.dev codespace

---
+++ project/app.py
@@ -0,0 +1,31 @@
+import os
+
+from cs50 import SQL
+from datetime import datetime
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+# TODO: use a different SQL library than the one cs50 provides
+# db = SQL("sqlite:///finance.db")
+
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+    return render_template("login.html")
