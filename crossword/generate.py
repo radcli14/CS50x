8673fc8a1e0c1119670baff93f4cc02f034b49a3
radcli14 <radcli14@gmail.com>
2025-11-30 12:04:35 +0100

    wrote assignment_complete

--- crossword/generate.py
+++ crossword/generate.py
@@ -190,7 +190,10 @@ class CrosswordCreator():
         Return True if `assignment` is complete (i.e., assigns a value to each
         crossword variable); return False otherwise.
         """
-        raise NotImplementedError
+        for variable in self.crossword.variables:
+            if variable not in assignment:
+                return False
+        return True
 
     def consistent(self, assignment):
         """
